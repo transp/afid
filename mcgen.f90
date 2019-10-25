e473bba7c6bf6404d78533d171251a7a888ae92b
Joshua Breslau <jbreslau@pppl.gov>
2019-10-25 11:51:38 -0400

    Accelerated mcgen by buffering data for NetCDF writes.

--- mcgen.f90
+++ mcgen.f90
@@ -122,15 +122,18 @@ SUBROUTINE writejac(froot, nparts, Rmin, Rmax, zmin, zmax, &
   REAL(KIND=rspec), INTENT(IN) :: vlmin, vlmax, vpsmax
   REAL(KIND=rspec), INTENT(IN) :: ioncharge, ionmass
 
+  INTEGER, PARAMETER :: nfbufsize = 128, interval = 250000
+
+  REAL(KIND=rspec), ALLOCATABLE, DIMENSION(:) :: pphi, mu, Etot
   REAL(KIND=rspec) buffer(3)
   REAL(KIND=rspec) zh, dz, rh, modB, vlh, dvl, vph, bphi
-  REAL(KIND=rspec) mot, pmag, mu, Etot, pphi
+  REAL(KIND=rspec) mot, pmag, tmp
   REAL(KIND=rspec) rhs, drs, vphs, psi_lcfs, psiw
   REAL x
-  INTEGER ids(3), dimids(2)
-  INTEGER ierr, ipart, isv, cpart, minseed
+  INTEGER ids(3), dimids(2), strt(2), cnt(2)
+  INTEGER ierr, ipart, jbuf, minseed, iplast
   INTEGER ncid, ptcdim, specdim, ppid, muid, Eid, wtid, lid
-  INTEGER, DIMENSION(:), ALLOCATABLE :: seed
+  INTEGER, DIMENSION(:), ALLOCATABLE :: isv, seed
 
   PRINT *,'STARTING JAC'
 
@@ -142,21 +145,20 @@ SUBROUTINE writejac(froot, nparts, Rmin, Rmax, zmin, zmax, &
   drs = Rmax**2 - Rmin**2
   dvl = vlmax - vlmin
   mot = 0.5*ionmass
-  !nparts = 16000000
 
   ! Get max flux (at lcfs)
-  CALL ps_intrp_2d(ps%R_min_lcfs, ps%z_axis, ps%id_PsiRZ, mu, ierr)
+  CALL ps_intrp_2d(ps%R_min_lcfs, ps%z_axis, ps%id_PsiRZ, tmp, ierr)
   CALL ckerr(ierr,'ps_intrp_2d')
   CALL ps_intrp_2d(ps%R_max_lcfs, ps%z_axis, ps%id_PsiRZ, psi_lcfs, ierr)
   CALL ckerr(ierr,'ps_intrp_2d')
-  IF (mu.GT.psi_lcfs) psi_lcfs = mu
+  IF (tmp.GT.psi_lcfs) psi_lcfs = tmp
   PRINT *,'psi_lcfs = ',psi_lcfs
-  CALL ps_intrp_2d(ps%R_axis, ps%z_axis, ps%id_PsiRZ, mu, ierr)
+  CALL ps_intrp_2d(ps%R_axis, ps%z_axis, ps%id_PsiRZ, tmp, ierr)
   CALL ckerr(ierr,'ps_intrp_2d')
-  PRINT *,'psi_axis = ',mu
-  CALL ps_intrp_2d(ps%R_axis, ps%z_axis, ps%id_BphiRZ, mu, ierr)
+  PRINT *,'psi_axis = ',tmp
+  CALL ps_intrp_2d(ps%R_axis, ps%z_axis, ps%id_BphiRZ, tmp, ierr)
   CALL ckerr(ierr,'ps_intrp_2d')
-  PRINT *,'B_phi_axis = ',mu
+  PRINT *,'B_phi_axis = ',tmp
   psiw = ioncharge*psi_lcfs
 
   ! Create output file
@@ -183,12 +185,16 @@ SUBROUTINE writejac(froot, nparts, Rmin, Rmax, zmin, zmax, &
   ierr = nf90_def_var(ncid, 'sgn_v', NF90_BYTE, dimids, lid)
   ierr = nf90_enddef(ncid)
 
+  ALLOCATE(pphi(nfbufsize), mu(nfbufsize), Etot(nfbufsize), isv(nfbufsize))
+
   CALL RANDOM_SEED(SIZE=minseed)
   PRINT *,'Min random seed len = ',minseed
   ALLOCATE(seed(minseed))
   seed = 1
   CALL RANDOM_SEED(PUT=seed)
-  ipart = 0; cpart = 1
+  ipart = 0; jbuf = 1; iplast = 0
+  strt = (/ 1, 1 /);  cnt = (/ 1, nfbufsize /)
+  PRINT *,'I/O buffer size = ',nfbufsize
   PRINT *,'Generating',nparts,'random particles within LCFS...'
   DO
      !Choose random real space coordinates
@@ -211,39 +217,50 @@ SUBROUTINE writejac(froot, nparts, Rmin, Rmax, zmin, zmax, &
      vlh = vlmin + x*dvl
 
      !Convert to constants of motion
-     CALL ps_intrp_2d(rh, zh, ids, buffer, ierr)
+     CALL ps_intrp_2d(rh, zh, ids, buffer, ierr) !Retrieve magnetic field
      CALL ckerr(ierr,'ps_intrp_2d')
      modB = SQRT(buffer(1)**2 + buffer(2)**2 + buffer(3)**2)
      bphi = buffer(3)/modB
 
-     Etot = mot*(vphs + vlh**2)
-     mu = mot*vphs/modB
-     pphi = ioncharge*pmag - ionmass*rh*vlh*bphi  !J.B. sign of vlh term
-     !pphi = ioncharge*pmag + ionmass*rh*vlh*bphi  !D.L. sign of vlh term
+     Etot(jbuf) = mot*(vphs + vlh**2)
+     mu(jbuf) = mot*vphs/modB
+     pphi(jbuf) = ioncharge*pmag - ionmass*rh*vlh*bphi  !J.B. sign of vlh term
+     !pphi(jbuf) = ioncharge*pmag + ionmass*rh*vlh*bphi  !D.L. sign of vlh term
 
      !Classify orbit
      if (vlh.GT.0.0) THEN
-        isv = 1
+        isv(jbuf) = 1
      ELSE
-        isv = -1
+        isv(jbuf) = -1
      ENDIF
 
-     !Write to NetCDF
-     ierr = nf90_put_var(ncid, ppid, pphi, (/1, cpart/))
-     ierr = nf90_put_var(ncid, muid, mu, (/1, cpart/))
-     ierr = nf90_put_var(ncid, Eid, Etot, (/1, cpart/))
-     ierr = nf90_put_var(ncid, wtid, 1.0, (/1, cpart/))
-     ierr = nf90_put_var(ncid, lid, isv, (/1, cpart/))
-     IF (ierr.NE.NF90_NOERR) PRINT *,TRIM(nf90_strerror(ierr))
-     cpart = cpart + 1
+     jbuf = jbuf + 1;  ipart = ipart + 1
+     IF ((jbuf.GT.nfbufsize).OR.(ipart.GE.nparts)) THEN
+        cnt(2) = jbuf - 1
+        IF (cnt(2).GT.0) THEN
+           !Flush buffer to NetCDF
+           ierr = nf90_put_var(ncid, ppid, pphi, start=strt, count=cnt)
+           ierr = nf90_put_var(ncid, muid, mu,   start=strt, count=cnt)
+           ierr = nf90_put_var(ncid, Eid,  Etot, start=strt, count=cnt)
+           ierr = nf90_put_var(ncid, lid,  isv,  start=strt, count=cnt)
+           Etot(1:cnt(2)) = 1.0
+           ierr = nf90_put_var(ncid, wtid, Etot, start=strt, count=cnt)
+           IF (ierr.NE.NF90_NOERR) PRINT *,TRIM(nf90_strerror(ierr))
+           strt(2) = strt(2) + cnt(2)
+           jbuf = 1
+        ENDIF
+     ENDIF
 
-     ipart = ipart + 1
-     IF (MOD(ipart,500000).EQ.0) PRINT *,ipart
+     IF (MOD(ipart,interval).EQ.0) THEN
+        IF (ipart.NE.iplast) PRINT *,ipart
+        iplast = ipart
+     ENDIF
+
      IF (ipart.GE.nparts) EXIT
   END DO
-  PRINT *,cpart-1,' total particles written.'
+  PRINT *,strt(2)-1,' total particles written.'
 
-  DEALLOCATE(seed)
+  DEALLOCATE(pphi, mu, Etot, isv, seed)
 
   !CLOSE(9)
   ierr = nf90_close(ncid)
